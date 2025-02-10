2e98fc734fb3c4867684e1019794c1ba65f56cd1
Tanvir <tanvir.info07@gmail.com>
2025-02-10 23:27:34 +0600

    cash disable

--- utils/fetchDataFromMongodb.js
+++ utils/fetchDataFromMongodb.js
@@ -2,7 +2,10 @@ import { replaceBlogsArrayId } from "./modifyMongodbId";
 
 export const getBlogs = async () => {
   try {
-    const res = await fetch(`${process.env.LOCAL_URL}/blogs`);
+    const res = await fetch(`${process.env.LOCAL_URL}/blogs`, {
+      cache: "no-store", // Disable caching
+    });
+
     const data = await res.json();
     if (data.success) {
       return replaceBlogsArrayId(data?.blogs) || [];
@@ -11,9 +14,13 @@ export const getBlogs = async () => {
     return { status: 501, message: "Blog Data fetch error" };
   }
 };
-export const getBlogbyId = async (id) => {
+
+export const getBlogById = async (id) => {
   try {
-    const res = await fetch(`${process.env.LOCAL_URL}/blogs/${id}`);
+    const res = await fetch(`${process.env.LOCAL_URL}/blogs/${id}`, {
+      cache: "no-store", // Disable caching
+    });
+
     const data = await res.json();
     return data || {};
   } catch (error) {
