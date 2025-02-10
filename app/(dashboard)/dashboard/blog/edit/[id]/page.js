4110f85b4f88e9dd528edfc89e66325b564b47cf
Tanvir <tanvir.info07@gmail.com>
2025-02-10 23:38:19 +0600

    no-store use

--- app/(dashboard)/dashboard/blog/edit/[id]/page.js
+++ app/(dashboard)/dashboard/blog/edit/[id]/page.js
@@ -1,11 +1,11 @@
 import React, { Suspense } from "react";
-import { getBlogbyId } from "@/utils/fetchDataFromMongodb";
+import { getBlogById } from "@/utils/fetchDataFromMongodb";
 import BlogAddForm from "@/app/(dashboard)/_components/blogs/BlogAddForm";
 import DotLoader from "@/components/common/DotLoader";
 
 const BlogEditPage = async ({ params }) => {
   const { id } = await params;
-  const blog = await getBlogbyId(id);
+  const blog = await getBlogById(id);
   return (
     <Suspense fallback={<DotLoader />}>
       <BlogAddForm blog={blog} />
